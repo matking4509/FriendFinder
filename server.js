783dd0c1a1606a6865c48db075a6e5ed08edb1b0
Mat King <mat@matking.info>
2019-02-18 13:16:15 -0500

    Continued Frameing.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+var friends = require("./app/data/friends");
 
 // Sets up the Express App
 // =============================================================
@@ -12,46 +13,16 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//*
-app.get("/", function(req, res) {
-    // res.send("Welcome to the Star Wars Page!")
-    res.sendFile(path.join(__dirname, "index.html"));
-});
-//*
-app.get("/api/curTables", function(req, res) {
-    // res.send("Welcome to the Star Wars Page!")
-    res.json(dinerInfo);
-});
-//*
-app.post("/api/makeRes", function(req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
-    if (dinerInfo.length >= maxTables) {
-      var waitlistRes = req.body;
-      waitlistInfo.push(waitlistRes);
-      console.log("waitlist",waitlistRes);
-    } else {
-      var newRes = req.body;
-      
-      // Randomly Select Table
-      randomTable();
-      newRes.table = rTn;
-      console.log("reserved",newRes);
-    
-      // We then add the json the user sent to the character array
-      dinerInfo.push(newRes);
-    
-      // We then display the JSON to the users
-      
-      
-    }
-    res.json(dinerInfo);
-    // res.json(waitlistInfo);
-});
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
 
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
-
-//Server Start
+//Start Server Listener
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
