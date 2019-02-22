5c00fe14db72d42e193a536ff622b1e9215f2b7d
Mat King <mat@matking.info>
2019-02-21 22:26:31 -0500

    Final

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,4 @@
-// 5. You should save your application's data inside of `app/data/friends.js` as an array of objects. Each of these objects should roughly follow the format below.
+// Loaded in a seed person, so everyone has a friend.
 var friendArray =
 [
     {
